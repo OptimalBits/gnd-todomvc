8bf704eac41c4bf02f5a922eca14c0fd16bc949c
Manuel Astudillo <manuel@optimalbits.com>
2013-01-01 23:12:30 +0100

    Updated to use the new gnd typescript version.

--- todo/js/app.js
+++ todo/js/app.js
@@ -1,28 +1,52 @@
-curl( ['ginger', 
-       'ginger/route',
-       'js/controllers/todo',
-       'js/controllers/todoList',
-       'js/models/todo',] , function( ginger , route, todoView, todoList, Todo ) {
-   
-   var list;
-   ginger.Collection.instantiate(Todo, [],function(err,collection){
-     list = new todoList(collection, {});
-     list.render($('#container'));
-     list.createTodo( { description : 'Write a book' , completed : false } );
-     list.createTodo( { description : 'Plant a tree' , completed : true } );
-     list.createTodo( { description : 'Coniferos' , completed : true } );
-   })
-  route(function(req) {
-    req.get(function() {
-      req.get(':state', '', function() {
-        var state = this.params.state;
-        if( state == 'completed' ) {
-          list.showCompleted();
-        } else {
-          list.showActive();          
+curl(['gnd', 'js/controllers/todoList'], function(Gnd, TodoListCtrl) {
+  var 
+    Todo = Gnd.Model.extend('todos'),
+    TodoList = Gnd.Model.extend('todolists'),
+    todoList = new TodoList(),
+    storageLocal  = new Gnd.Storage.Local();
+    app = new Gnd.Base();
+  
+  Gnd.Model.storageQueue = new Gnd.Storage.Queue(storageLocal);
+    
+  function setFilter(all, active, completed){
+    app.set('filterAll', all);
+    app.set('filterActive', active);
+    app.set('filterCompleted', completed);
+  }
+  
+  setFilter(true, false, false);
+  
+  var appViewModel = new Gnd.ViewModel($('#filters')[0], {app: app});
+  
+  todoList.all(Todo, function(err, todos){
+    var list = new TodoListCtrl(todos);
+     
+    list.createTodo({description: 'Write a book', completed: false});
+    list.createTodo({description: 'Plant a tree', completed: true});
+    list.createTodo({description: 'Coniferos', completed: true});
+     
+    Gnd.Route.listen(function(req) {
+      req.get(function() {
+        
+        if(req.isLast()){
+          list.showAll();
+          setFilter(true, false, false);
         }
-        $('a', list.$filters).removeClass('selected').filter('[href="#/' + state + '"]').addClass('selected');
-      });
+        
+        req.get(':state', '', function() {
+          var state = this.params.state;
+          switch(state){
+            case 'active': 
+              list.showActive();
+              setFilter(false, true, false);
+              break;
+            case 'completed': 
+              list.showCompleted();
+              setFilter(false, false, true);
+              break;
+          }
+        });
+      })
     })
   })
 });
--- todo/js/controllers/todoList.js
+++ todo/js/controllers/todoList.js
@@ -1,109 +1,112 @@
-define( ['ginger',
-         'js/models/todo',
-         'js/controllers/todo'] , function( ginger , Todo , todoView) {
-  var todoList = ginger.View.extend({
-    constructor : function List(collection){
-      this.super(List, 'constructor');
+define(['gnd'], function(Gnd) {
+return Gnd.Util.extend(Gnd.Base, function(_super){
+  var Todo = Gnd.Model.extend('todos');
   
+  return {
+    constructor : function TodoList(collection){
       var self = this;
+      _super.constructor.call(self);
+
       self.set('collection', collection);
       
-      self.$el = $('#todoapp');
-      self.statsTemplate = _.template( $('#stats-template').html() );
-      
-      self.$headerInput = $('#new-todo');
-      self.$toggleAll = $('#toggle-all');
-      self.$todosContainer = $('#todo-list');
-      
-      // events
-      self.$headerInput.keyup(function( e ){
-        if( e.which === 13 ) {
-          var description = $.trim(self.$headerInput.val());
-          if ( description != '' ) {
-            var newtodo = { description : description };
-            self.createTodo( newtodo );
-            self.$headerInput.val('');
-          }
-        }
-      })
-
-      self.$toggleAll.on('click', function(){
-        var inputEl = this;
-        $.each( self.collection.items , function( i , todo ) {
-          todo.set( 'completed' , inputEl.checked );
-        })
-      })
-      
-      // listeners
-      self.collection.on( 'added: updated: removed:' , function( todo ) {
+      self.collection.on('added: updated: removed:', function(todo) {
         self.updateCompleted();
         self.updateCounter();
-      })
-    },
-    render : function( parent ) {
-			$('#footer').html(this.statsTemplate({
-				completed: 0,
-				remaining:0
-			}));
-      this.$todoCount = $('#todo-count');
-      this.$clearComplete = $('#clear-completed');
-      self = this;
-      self.$clearComplete.on( 'click' , function(){
-        $.each( self.collection.items , function( i , todo ) {
-          if( todo.completed ) {
-            todo.destroy();
-          }
-        })
-      })
-    },
-    createTodo : function( data ) {
-      var model = new Todo( data );
-      model.keepSynced();
-      this.collection.add( model );
-      var view = new todoView( model );
-      view.render( this.$todosContainer );
-      var self = this;
+      });
+           
+      this.remaining = 0;
+      this.completed = 0;
       
-      view.on('completed',function(){
-        self.updateCompleted();
-      })
-     /* var todoView = new todoView( todo );
-      todo.render( this.$todosContainer );
-      todo.on('completed',function(){
-        self.updateCompleted();
-      })*/
+      var todoListViewModel = new Gnd.ViewModel($('#todoapp')[0], {
+        todolist: this,
+        todos: collection
+      });      
     },
-    add : function( model ) {
-      var self = this;
-      var todo = new todoView( model );
-      todo.render( this.$todosContainer );
-      todo.on('completed',function(){
-        self.updateCompleted();
-      })
+    
+    createTodo: function(args){
+      var model = new Todo(args);
+      this.collection.add(model);
     },
+    
+    //
+    // Event Handlers
+    //
+    clearCompleted: function(node, evt){
+      var itemIds = [];
+      this.collection.each(function(item){
+        if(item.completed){
+          itemIds.push(item.id());
+        }
+      });
+      this.collection.remove(itemIds);
+    },
+    toggleAll: function(node, evt) {
+      this.collection.each(function(todo) {
+        todo.set('completed', node.checked);
+      });
+    },
+    addTodo: function(node, evt) {
+      if(evt.which === 13) {
+        var description = $.trim(node.value);
+        if ( description != '' ){
+          node.value = '';
+          this.createTodo({description : description});
+        }
+      }
+    },
+    removeTodo: function(node, evt){
+      var todoNode = node.parentNode.parentNode;
+      this.collection.remove(todoNode.getAttribute('data-item'));
+    },
+    startEditing: function(node, evt){
+      var todoNode = node.parentNode.parentNode;
+      var todo = this.collection.find(function(item){
+        return item.id() === todoNode.getAttribute('data-item');
+      });
+      todo.set('isEditing', true);
+    },
+    endEditing: function(node, evt){
+      if(evt.which === 13 || evt.type === 'blur'){
+        var todoNode = node.parentNode;
+        var todo = this.collection.find(function(item){
+          return item.id() === todoNode.getAttribute('data-item');
+        });
+        todo.set('isEditing', false);
+      }
+    },
+    
+    //
+    // Stats updaters
+    //
     updateCounter : function() {
-      var itemsLeft = this.collection.filter(function( val ) {
+      var itemsLeft = this.collection.filter(function(val) {
         return !val.completed;
       })
-      switch ( itemsLeft.length ) {
-        case 1 :
-          this.$todoCount.html('<b>1</b> item left');
-          break;
-        default :
-          this.$todoCount.html('<b>'+ itemsLeft.length +'</b> items left');
-      }
+      this.set('remaining', itemsLeft.length);
     },
     updateCompleted : function(){
-      var itemsCompleted = this.collection.filter(function( val ) {
+      var itemsCompleted = this.collection.filter(function(val){
         return val.completed;
-      })
-      if( itemsCompleted ) {
-        this.$clearComplete.text('Clear completed (' + itemsCompleted.length + ')');
-        this.$clearComplete.show();
-      } else {
-        this.$clearComplete.hide();
-      }
+      });
+      this.set('completed', itemsCompleted.length);
     },
-  })
-  return todoList;
+    
+    //
+    // Filters
+    //
+    showAll: function(){
+      this.collection.set('filterFn', null);
+    },
+    showCompleted: function(){
+      this.collection.set('filterFn', function(item){
+        return !!item.completed;
+      });
+    },
+    showActive: function(){
+      this.collection.set('filterFn', function(item){
+        return !item.completed;
+      });
+    }
+  }
+})
 });
